a0c037f36e89e7996373d638e29980624e833fc1
VIVEK Aggarwal <vivek@VIVEKs-MacBook-Air.local>
2025-02-14 17:39:39 +0530

    done hai

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+
 const VideoRedirect = () => {
     const navigate = useNavigate();
     
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"
 import VideoRedirect from "./App.jsx";
 import "./index.css";
 
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<VideoRedirect />} />
       </Routes>
+      <Analytics />
     </Router>
   </React.StrictMode>
 );
